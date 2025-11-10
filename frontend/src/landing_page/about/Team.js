d81197a6fa3d474449efb67559469197c10b7795
saif sanadi <saifsanadi99@gmail.com>
2025-11-10 12:24:35 +0530

    Update Team.js: Replace Vinayak with Saif throughout

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -13,15 +13,15 @@ function Team() {
       >
         <div className="col-6 p-3 text-center">
           <img
-            src="media/images/Vinayak.jpeg"
+            src="media/images/Saif.jpeg"
             style={{ borderRadius: "100%", width: "50%" }}
           />
-          <h4 className="mt-5">Vinayak Agasimani</h4>
+          <h4 className="mt-5">Saif Sanadi</h4>
           <h6>Developer</h6>
         </div>
         <div className="col-6 p-3">
           <p>
-            Vinayak is an innovator and tech enthusiast, building solutions that merge electronics, communication, and real-world problem solving. From IoT-based systems to full-stack applications, he works on projects that aim to create practical impact.
+            Saif is an innovator and tech enthusiast, building solutions that merge electronics, communication, and real-world problem solving. From IoT-based systems to full-stack applications, he works on projects that aim to create practical impact.
           </p>
           <p>
             He is the Vice Chair of the IEEE Student Branch at JCE, actively driving initiatives, workshops, and collaborations in the tech community.
